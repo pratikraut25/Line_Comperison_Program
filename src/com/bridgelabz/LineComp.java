6ace831b5062fa7ad79bb50d33a44b30d0f7fa9f
pratikraut25 <pratikraut2512@gmail.com>
2022-10-15 22:22:30 +0530

    Added: Welcome Messege

---
+++ src/com/bridgelabz/LineComp.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class LineComp {
+
+	public static void main(String[] args) {
+         System.out.println("Welcome to Line Comperison Computation Program");
+		
+	}
+
+}
